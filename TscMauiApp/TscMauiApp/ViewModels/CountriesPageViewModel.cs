6a4b31578a8ea742549011e5b3943a70c1cd1f0b
Nicolás Mojarro <mojazu25@gmail.com>
2024-09-12 16:06:54 -0600

    Load list of countries.

--- TscMauiApp/TscMauiApp/ViewModels/CountriesPageViewModel.cs
+++ TscMauiApp/TscMauiApp/ViewModels/CountriesPageViewModel.cs
@@ -1,12 +1,41 @@
 using System;
+using CommunityToolkit.Mvvm.ComponentModel;
+using TscMauiApp.Models;
 using TscMauiApp.Services.Interfaces;
 
 namespace TscMauiApp.ViewModels;
 
 public partial class CountriesPageViewModel : BaseViewModel
 {
-    public CountriesPageViewModel(INavigationService navigationService, IDialogService dialogService) 
+    private readonly ICountryService _countryService;
+
+    [ObservableProperty]
+    private List<Country> _countries;
+
+    public CountriesPageViewModel(INavigationService navigationService, IDialogService dialogService, ICountryService countryService)
         : base(navigationService, dialogService)
     {
+        _countryService = countryService;
+
+        _ = LoadCountries();
+    }
+
+    private async Task LoadCountries()
+    {
+        try
+        {
+            IsBusy = true;
+            var countries = await _countryService.GetCountries();
+            IsBusy = false;
+
+            if (countries != null)
+            {
+                Countries = countries;
+            }
+        }
+        catch (Exception)
+        {
+            await _dialogService.ShowAlert("Error", "An error has occurred.", "OK");
+        }
     }
 }
