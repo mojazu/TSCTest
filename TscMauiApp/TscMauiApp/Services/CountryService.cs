bceb2e2622668fdeda7290a30d9fe935ac8030b6
Nicolás Mojarro <mojazu25@gmail.com>
2024-09-12 18:19:16 -0600

    Added service calls for add, edit and delete country.

--- TscMauiApp/TscMauiApp/Services/CountryService.cs
+++ TscMauiApp/TscMauiApp/Services/CountryService.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text;
 using Newtonsoft.Json;
 using TscMauiApp.Models;
 using TscMauiApp.Services.Interfaces;
@@ -9,6 +10,7 @@ public class CountryService : ICountryService
 {
     private const string BaseUrl = "https://api-exam.tsc-dev.xyz";
     private const string EndpointCountries = "/countries";
+    private const string EndpointCountry = "/countries/{0}";
     private const string EndpointStates = "/countries/{0}/states";
 
     private HttpClient _client;
@@ -60,5 +62,58 @@ public class CountryService : ICountryService
             throw;
         }
         return subdivisions;
+    }
+
+    public async Task<bool> AddCountry(Country country)
+    {
+        var uri = new Uri($"{BaseUrl}{EndpointCountries}");
+        try
+        {
+            string json = JsonConvert.SerializeObject(country);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _client.PostAsync(uri, content);
+
+            return response.IsSuccessStatusCode;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    public async Task<bool> EditCountry(Country country)
+    {
+        var countryUri = string.Format(EndpointCountry, country.Id);
+        var uri = new Uri($"{BaseUrl}{countryUri}");
+        try
+        {
+            string json = JsonConvert.SerializeObject(country);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await _client.PutAsync(uri, content);
+
+            return response.IsSuccessStatusCode;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
+    }
+
+    public async Task<bool> DeleteCountry(int countryId)
+    {
+        var countryUri = string.Format(EndpointCountry, countryId);
+        var uri = new Uri($"{BaseUrl}{countryUri}");
+        try
+        {
+            HttpResponseMessage response = await _client.DeleteAsync(uri);
+            return response.IsSuccessStatusCode;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            return false;
+        }
     }
 }
